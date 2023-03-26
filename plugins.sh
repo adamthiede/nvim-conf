e32df12e4884473192a5e5302b5283c4f2e715e2
Adam Thiede <adamj@mailbox.org>
2023-03-26 06:07:36 -0500

    plugins variable

--- plugins.sh
+++ plugins.sh
@@ -2,10 +2,11 @@
 set -e
 plugdir="$HOME/.local/share/nvim/site/pack/plugins/start"
 optdir="$HOME/.local/share/nvim/site/pack/plugins/opt"
+plugins="https://github.com/neovim/nvim-lspconfig https://github.com/preservim/nerdtree https://github.com/vala-lang/vala.vim https://github.com/tpope/vim-fugitive https://github.com/simrat39/rust-tools.nvim https://github.com/ibhagwan/fzf-lua"
 mkdir -p $plugdir
 mkdir -p $optdir
 cd $plugdir
-for plugin in https://github.com/neovim/nvim-lspconfig https://github.com/preservim/nerdtree https://github.com/vala-lang/vala.vim https://github.com/tpope/vim-fugitive https://github.com/simrat39/rust-tools.nvim; do
+for plugin in $plugins; do
 	plugname=${plugin##*/}
 	echo $plugname
 	if [ -d $plugname ];then
